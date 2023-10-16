fbe5fb6380aae75992d36913536660e25af2f081
“Kimberly <kchaparro@ucdavis.edu>
2023-10-16 12:11:28 -0700

    update dynamic page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import { Container } from "@chakra-ui/react";
 import AddTodo from "../components/AddTodo";
-import AddInspoQuote from "../components/AddInspoQuote";
+import AddEvent from "../components/AddEvent";
 import Auth from "../components/Auth";
 import TodoList from "../components/TodoList";
-import InspoList from "../components/InspoList";
+import EventList from "../components/EventList";
 export default function Home() {
   return (
     <Container maxW="7xl">
       <Auth />
       <AddTodo />
-      <AddInspoQuote />
+      <AddEvent />
       <TodoList />
-      <InspoList />
+      <EventList />
     </Container>
   );
 }
