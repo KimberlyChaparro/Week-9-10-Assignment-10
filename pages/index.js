59b2d0b20444440d6a5099c0ba2829c5c9141b6d
“Kimberly <kchaparro@ucdavis.edu>
2023-10-16 13:55:33 -0700

    adjusting firebase data naming

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import { Container } from "@chakra-ui/react";
 import AddTodo from "../components/AddTodo";
 import AddEvent from "../components/AddEvent";
+import AddContact from "../components/AddContact";
 import Auth from "../components/Auth";
 import TodoList from "../components/TodoList";
 import EventList from "../components/EventList";
+import ContactList from "../components/ContactList";
 export default function Home() {
   return (
     <Container maxW="7xl">
       <Auth />
       <AddTodo />
       <AddEvent />
+      <AddContact />
       <TodoList />
       <EventList />
+      <ContactList />
     </Container>
   );
 }
